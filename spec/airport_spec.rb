a383df843d7094c476ed63e57293d8b4e1a2f9d7
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-29 16:17:12 +0000

    Plane has a flying status

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,12 +1,9 @@
 require 'airport'
 require 'plane'
-require 'weather'
- 
+
 # A plane currently in the airport can be requested to take off.
-#
 # No more planes can be added to the airport, if it's full.
 # It is up to you how many planes can land in the airport and how that is impermented.
-#
 # If the airport is full then no planes can land
 describe Airport do
   let(:airport) { Airport.new }
@@ -39,27 +36,7 @@ describe Airport do
     end
   end
 end
-# When we create a new plane, it should have a "flying" status, thus planes can not be created in the airport.
-#
-# When we land a plane at the airport, the plane in question should have its status changed to "landed"
-#
-# When the plane takes of from the airport, the plane's status should become "flying"
-describe Plane do
- 
-  let(:plane) { Plane.new }
-  
-  it 'has a flying status when created' do
-  end
-  
-  it 'has a flying status when in the air' do
-  end
-  
-  it 'can take off' do
-  end
-  
-  it 'changes its status to flying after taking of' do
-  end
-end
+
  
 # grand final
 # Given 6 planes, each plane must land. When the airport is full, every plane must take off again.
